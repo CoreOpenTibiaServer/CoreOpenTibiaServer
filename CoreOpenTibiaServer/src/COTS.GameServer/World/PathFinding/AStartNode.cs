b437d0318052fd7cf13a684519727e07b6b2df0d
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-21 23:06:52 -0200

    Created turbo-nitro PriorityQueue

---
+++ CoreOpenTibiaServer/src/COTS.GameServer/World/PathFinding/AStartNode.cs
@@ -0,0 +1,9 @@
+﻿namespace COTS.GameServer.World.PathFinding {
+
+    public sealed class AStartNode {
+        public readonly uint X;
+        public readonly uint Y;
+        public float Priority;
+        public int QueueIndex;
+    }
+}
