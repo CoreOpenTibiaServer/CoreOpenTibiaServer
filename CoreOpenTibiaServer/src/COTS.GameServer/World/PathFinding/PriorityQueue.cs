4f78d114e3b960490850d970b55024c0eeb845c5
Mirandatz <hardly.a.challenge@gmail.com>
2018-01-27 12:45:11 -0200

    Finished implementing the path finding algorithm. It still isn't fully functional since it depends on the World class.

--- CoreOpenTibiaServer/src/COTS.GameServer/World/PathFinding/PriorityQueue.cs
+++ CoreOpenTibiaServer/src/COTS.GameServer/World/PathFinding/PriorityQueue.cs
@@ -10,7 +10,7 @@ namespace COTS.GameServer.World.PathFinding {
     /// </summary>
     public sealed class PriorityQueue {
         private int _numNodes;
-        private AStartNode[] _nodes;
+        private readonly AStartNode[] _nodes;
 
         public PriorityQueue(int maxNodes) {
 #if DEBUG
@@ -23,10 +23,6 @@ namespace COTS.GameServer.World.PathFinding {
             _nodes = new AStartNode[maxNodes + 1];
         }
 
-        /// <summary>
-        /// Returns the number of nodes in the queue.
-        /// O(1)
-        /// </summary>
         public int Count => _numNodes;
 
         /// <summary>
@@ -34,16 +30,6 @@ namespace COTS.GameServer.World.PathFinding {
         /// attempting to enqueue another item will cause undefined behavior.  O(1)
         /// </summary>
         public int MaxSize => _nodes.Length - 1;
-
-        /// <summary>
-        /// Removes every node from the queue.
-        /// O(n) (So, don't do this often!)
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Clear() {
-            Array.Clear(_nodes, 1, _numNodes);
-            _numNodes = 0;
-        }
 
         /// <summary>
         /// Returns (in O(1)!) whether the given node is in the queue.  O(1)
@@ -62,14 +48,8 @@ namespace COTS.GameServer.World.PathFinding {
             return (_nodes[node.QueueIndex] == node);
         }
 
-        /// <summary>
-        /// Enqueue a node to the priority queue.  Lower values are placed in front. Ties are broken arbitrarily.
-        /// If the queue is full, the result is undefined.
-        /// If the node is already enqueued, the result is undefined.
-        /// O(log n)
-        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Enqueue(AStartNode node, int priority) {
+        public bool TryEnqueue(AStartNode node) {
 #if DEBUG
             if (node == null)
                 throw new ArgumentNullException(nameof(node));
@@ -79,20 +59,56 @@ namespace COTS.GameServer.World.PathFinding {
                 throw new InvalidOperationException("Node is already enqueued: " + node);
 #endif
 
-            node.Priority = priority;
+            if (_numNodes >= MaxSize)
+                return false;
+            
             _numNodes++;
             _nodes[_numNodes] = node;
             node.QueueIndex = _numNodes;
             CascadeUp(node);
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryDequeue(out AStartNode node) {
+#if DEBUG
+            if (_numNodes <= 0)
+                throw new InvalidOperationException("Cannot call Dequeue() on an empty queue");
+
+            if (!IsValidQueue()) {
+                throw new InvalidOperationException("Queue has been corrupted (Did you update a node priority manually instead of calling UpdatePriority()?" +
+                                                    "Or add the same node to two different queues?)");
+            }
+#endif
+            if (_numNodes == 0) {
+                node = null;
+                return false;
+            }
+
+            var returnMe = _nodes[1];
+            //If the node is already the last node, we can remove it immediately
+            if (_numNodes == 1) {
+                _nodes[1] = null;
+                _numNodes = 0;
+                node = returnMe;
+                return true;
+            }
+
+            //Swap the node with the last node
+            var formerLastNode = _nodes[_numNodes];
+            _nodes[1] = formerLastNode;
+            formerLastNode.QueueIndex = 1;
+            _nodes[_numNodes] = null;
+            _numNodes--;
+
+            //Now bubble formerLastNode (which is no longer the last node) down
+            CascadeDown(formerLastNode);
+            node = returnMe;
+            return true;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void CascadeUp(AStartNode node) {
-#if DEBUG
-            if (node == null)
-                throw new ArgumentNullException(nameof(node));
-#endif
-
             //aka Heapify-up
             int parent;
             if (node.QueueIndex > 1) {
@@ -126,11 +142,6 @@ namespace COTS.GameServer.World.PathFinding {
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void CascadeDown(AStartNode node) {
-#if DEBUG
-            if (node == null)
-                throw new ArgumentNullException(nameof(node));
-#endif
-
             //aka Heapify-down
             int finalQueueIndex = node.QueueIndex;
             int childLeftIndex = 2 * finalQueueIndex;
@@ -248,13 +259,6 @@ namespace COTS.GameServer.World.PathFinding {
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool HasHigherPriority(AStartNode higher, AStartNode lower) {
-#if DEBUG
-            if (higher == null)
-                throw new ArgumentNullException(nameof(higher));
-            if (lower == null)
-                throw new ArgumentNullException(nameof(lower));
-#endif
-
             return (higher.Priority < lower.Priority);
         }
 
@@ -264,13 +268,148 @@ namespace COTS.GameServer.World.PathFinding {
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool HasHigherOrEqualPriority(AStartNode higher, AStartNode lower) {
-#if DEBUG
-            if (higher == null)
-                throw new ArgumentNullException(nameof(higher));
-            if (lower == null)
-                throw new ArgumentNullException(nameof(lower));
-#endif
             return (higher.Priority <= lower.Priority);
+        }
+
+        /// <summary>
+        /// This method must be called on a node every time its priority changes while it is in the queue.
+        /// <b>Forgetting to call this method will result in a corrupted queue!</b>
+        /// Calling this method on a node not in the queue results in undefined behavior
+        /// O(log n)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void UpdateNodeGCost(AStartNode node, int newGCost) {
+#if DEBUG
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+            if (!Contains(node))
+                throw new InvalidOperationException("Cannot call UpdatePriority() on a node which is not enqueued: " + node);
+#endif
+
+            node.G = newGCost;
+            OnNodeUpdated(node);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void OnNodeUpdated(AStartNode node) {
+            // Bubble the updated node up or down as appropriate
+            int parentIndex = node.QueueIndex >> 1;
+
+            if (parentIndex > 0 && HasHigherPriority(node, _nodes[parentIndex])) {
+                CascadeUp(node);
+            } else {
+                //Note that CascadeDown will be called if parentNode == node (that is, node is the root)
+                CascadeDown(node);
+            }
+        }
+
+        /// <summary>
+        /// Removes a node from the queue.  The node does not need to be the head of the queue.
+        /// If the node is not in the queue, the result is undefined.  If unsure, check Contains() first
+        /// O(log n)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Remove(AStartNode node) {
+#if DEBUG
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+            if (!Contains(node))
+                throw new InvalidOperationException("Cannot call Remove() on a node which is not enqueued: " + node);
+#endif
+
+            //If the node is already the last node, we can remove it immediately
+            if (node.QueueIndex == _numNodes) {
+                _nodes[_numNodes] = null;
+                _numNodes--;
+                return;
+            }
+
+            //Swap the node with the last node
+            var formerLastNode = _nodes[_numNodes];
+            _nodes[node.QueueIndex] = formerLastNode;
+            formerLastNode.QueueIndex = node.QueueIndex;
+            _nodes[_numNodes] = null;
+            _numNodes--;
+
+            //Now bubble formerLastNode (which is no longer the last node) up or down as appropriate
+            OnNodeUpdated(formerLastNode);
+        }
+
+        /// <summary>
+        /// <b>Should not be called in production code.</b>
+        /// Checks to make sure the queue is still in a valid state.  Used for testing/debugging the queue.
+        /// </summary>
+        public bool IsValidQueue() {
+            for (int i = 1; i < _nodes.Length; i++) {
+                if (_nodes[i] != null) {
+                    int childLeftIndex = 2 * i;
+                    if (childLeftIndex < _nodes.Length && _nodes[childLeftIndex] != null && HasHigherPriority(_nodes[childLeftIndex], _nodes[i]))
+                        return false;
+
+                    int childRightIndex = childLeftIndex + 1;
+                    if (childRightIndex < _nodes.Length && _nodes[childRightIndex] != null && HasHigherPriority(_nodes[childRightIndex], _nodes[i]))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        /*
+        /// <summary>
+        /// Enqueue a node to the priority queue. Lower values are placed in front. Ties are broken arbitrarily.
+        /// If the queue is full, the result is undefined.
+        /// If the node is already enqueued, the result is undefined.
+        /// O(log n)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Enqueue(AStartNode node, int priority) {
+#if DEBUG
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+            if (_numNodes >= _nodes.Length - 1)
+                throw new InvalidOperationException("Queue is full - node cannot be added: " + node);
+            if (Contains(node))
+                throw new InvalidOperationException("Node is already enqueued: " + node);
+#endif
+
+            node.Priority = priority;
+            _numNodes++;
+            _nodes[_numNodes] = node;
+            node.QueueIndex = _numNodes;
+            CascadeUp(node);
+        }
+
+        /// <summary>
+        /// Removes every node from the queue.
+        /// O(n) (So, don't do this often!)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear() {
+            Array.Clear(_nodes, 1, _numNodes);
+            _numNodes = 0;
+        }
+
+        /// <summary>
+        /// Enqueue a node to the priority queue.
+        /// If the queue is full, the result is undefined.
+        /// If the node is already enqueued, the result is undefined.
+        /// O(log n)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Enqueue(AStartNode node) {
+#if DEBUG
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+            if (_numNodes >= _nodes.Length - 1)
+                throw new InvalidOperationException("Queue is full - node cannot be added: " + node);
+            if (Contains(node))
+                throw new InvalidOperationException("Node is already enqueued: " + node);
+#endif
+
+            _numNodes++;
+            _nodes[_numNodes] = node;
+            node.QueueIndex = _numNodes;
+            CascadeUp(node);
         }
 
         /// <summary>
@@ -348,88 +487,6 @@ namespace COTS.GameServer.World.PathFinding {
                 return _nodes[1];
             }
         }
-
-        /// <summary>
-        /// This method must be called on a node every time its priority changes while it is in the queue.
-        /// <b>Forgetting to call this method will result in a corrupted queue!</b>
-        /// Calling this method on a node not in the queue results in undefined behavior
-        /// O(log n)
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void UpdatePriority(AStartNode node, int priority) {
-#if DEBUG
-            if (node == null)
-                throw new ArgumentNullException(nameof(node));
-            if (!Contains(node))
-                throw new InvalidOperationException("Cannot call UpdatePriority() on a node which is not enqueued: " + node);
-#endif
-
-            node.Priority = priority;
-            OnNodeUpdated(node);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void OnNodeUpdated(AStartNode node) {
-            // Bubble the updated node up or down as appropriate
-            int parentIndex = node.QueueIndex >> 1;
-
-            if (parentIndex > 0 && HasHigherPriority(node, _nodes[parentIndex])) {
-                CascadeUp(node);
-            } else {
-                //Note that CascadeDown will be called if parentNode == node (that is, node is the root)
-                CascadeDown(node);
-            }
-        }
-
-        /// <summary>
-        /// Removes a node from the queue.  The node does not need to be the head of the queue.
-        /// If the node is not in the queue, the result is undefined.  If unsure, check Contains() first
-        /// O(log n)
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Remove(AStartNode node) {
-#if DEBUG
-            if (node == null)
-                throw new ArgumentNullException(nameof(node));
-            if (!Contains(node))
-                throw new InvalidOperationException("Cannot call Remove() on a node which is not enqueued: " + node);
-#endif
-
-            //If the node is already the last node, we can remove it immediately
-            if (node.QueueIndex == _numNodes) {
-                _nodes[_numNodes] = null;
-                _numNodes--;
-                return;
-            }
-
-            //Swap the node with the last node
-            var formerLastNode = _nodes[_numNodes];
-            _nodes[node.QueueIndex] = formerLastNode;
-            formerLastNode.QueueIndex = node.QueueIndex;
-            _nodes[_numNodes] = null;
-            _numNodes--;
-
-            //Now bubble formerLastNode (which is no longer the last node) up or down as appropriate
-            OnNodeUpdated(formerLastNode);
-        }
-
-        /// <summary>
-        /// <b>Should not be called in production code.</b>
-        /// Checks to make sure the queue is still in a valid state.  Used for testing/debugging the queue.
-        /// </summary>
-        public bool IsValidQueue() {
-            for (int i = 1; i < _nodes.Length; i++) {
-                if (_nodes[i] != null) {
-                    int childLeftIndex = 2 * i;
-                    if (childLeftIndex < _nodes.Length && _nodes[childLeftIndex] != null && HasHigherPriority(_nodes[childLeftIndex], _nodes[i]))
-                        return false;
-
-                    int childRightIndex = childLeftIndex + 1;
-                    if (childRightIndex < _nodes.Length && _nodes[childRightIndex] != null && HasHigherPriority(_nodes[childRightIndex], _nodes[i]))
-                        return false;
-                }
-            }
-            return true;
-        }
+       */
     }
 }
