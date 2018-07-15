7b07fa5086660467a4ca2e8e9a258cfa0610e984
Mirandatz <Mirandatz@users.noreply.github.com>
2018-07-15 19:59:39 -0300

    Moved remaining string extension methods from Extensions to StringExtensions.

--- src/COMMO.Utilities/StringExtensions.cs
+++ src/COMMO.Utilities/StringExtensions.cs
@@ -1,4 +1,6 @@
 namespace COMMO.Utilities {
+	using System;
+	using System.Collections.Generic;
 	using System.Text;
 
 	public static class StringExtensions {
@@ -18,6 +20,61 @@ namespace COMMO.Utilities {
 
 			var result = new StringBuilder(instr.Length * n);
 			return result.Insert(0, instr, n).ToString();
+		}
+
+		/// <summary>
+		/// Converts a string to a byte array
+		/// </summary>
+		/// <returns></returns>
+		public static byte[] ToByteArray(this string s) {
+			var value = new List<byte>();
+			foreach (var c in s) {
+				value.Add(c.ToByte());
+			}
+
+			return value.ToArray();
+		}
+
+		/// <summary>Convert a string of hex digits (ex: E4 CA B2) to a byte array.</summary>
+		/// <param name="s">The string containing the hex digits (with or without spaces).</param>
+		/// <returns>Returns an array of bytes.</returns>
+		public static byte[] ToBytesAsHex(this string s) {
+			s = s.Replace(" ", string.Empty);
+			var buffer = new byte[s.Length / 2];
+			for (var i = 0; i < s.Length; i += 2) {
+				buffer[i / 2] = Convert.ToByte(s.Substring(i, 2), 16);
+			}
+
+			return buffer;
+		}
+
+		/// <summary>
+		/// Convert a string of hex digits to a printable string of characters.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string ToPrintableStringAsHex(this string value) {
+			byte[] temp = value.ToBytesAsHex();
+			var loc = string.Empty;
+			for (var i = 0; i < temp.Length; i++) {
+				loc += temp[i].ToPrintableChar();
+			}
+
+			return loc;
+		}
+
+		/// <summary>
+		/// Converts a hex string to a integer
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static int ToIntAsHex(this string value) {
+			byte[] bytes = value.ToBytesAsHex();
+			if (bytes.Length >= 2) {
+				return BitConverter.ToInt16(bytes, 0);
+			}
+
+			return int.MinValue;
 		}
 	}
 }
