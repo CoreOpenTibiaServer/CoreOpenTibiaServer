2106167209a2dbc8b43f98ac13d47695b95701d4
Mirandatz <Mirandatz@users.noreply.github.com>
2018-07-20 02:44:03 -0300

    Created a "lazy wordl loader wrapper".

--- src/COMMO.Server/Game.cs
+++ src/COMMO.Server/Game.cs
@@ -21,6 +21,7 @@ using COMMO.Server.Monsters;
 using COMMO.Server.Movement;
 using COMMO.Server.Notifications;
 using COMMO.Server.Scripting;
+using COMMO.Server.World;
 using COMMO.Server.World.PathFinding;
 using System;
 using System.Collections.Concurrent;
@@ -43,12 +44,12 @@ namespace COMMO.Server {
 		/// <summary>
 		/// Singleton instance of the <see cref="Game"/> class.
 		/// </summary>
-		private static readonly Lazy<Game> _gameInstance = new Lazy<Game>(() => new Game());
+		private static readonly Game _gameInstance = new Game();
 
 		/// <summary>
 		/// Gets the singleton instance of the <see cref="Game"/> class.
 		/// </summary>
-		public static Game Instance => Game._gameInstance.Value;
+		public static Game Instance => Game._gameInstance;
 
 		private readonly object _attackLock;
 		private readonly object _walkLock;
@@ -77,8 +78,8 @@ namespace COMMO.Server {
 			// _map = new Map.Map(new SectorMapLoader(ServerConfiguration.LiveMapDirectory));
 			var otbmWorldData = File.ReadAllBytes(@"C:\Source\forgottenserver-master\data\world\forgotten.otbm");
 			var relevantData = new Memory<byte>(otbmWorldData).Slice(4, otbmWorldData.Length - 4);
-			var pseudoWorldLoader = World.OTBMWorldLoader.LoadWorld(relevantData);
-			_map = new Map.Map(pseudoWorldLoader);
+			var lazyWorldLoaderWrapper = new LazyWorldWrapper(relevantData);
+			_map = new Map.Map(lazyWorldLoaderWrapper);
 
 			// Initialize game vars.
 			Status = WorldState.Creating;
