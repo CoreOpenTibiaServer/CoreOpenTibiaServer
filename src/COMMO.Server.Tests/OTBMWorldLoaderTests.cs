36ba38c2b97c0a41edf6910eae3bc0dc6456b0a9
MirandaTZ <>
2018-07-31 18:13:27 -0300

    Refactored WordlLoaderTests world loading mechanism.

--- src/COMMO.Server.Tests/OTBMWorldLoaderTests.cs
+++ src/COMMO.Server.Tests/OTBMWorldLoaderTests.cs
@@ -1,13 +1,32 @@
 namespace COMMO.Server.Tests {
 	using NUnit.Framework;
+	using Server.World;
 	using System;
+	using System.IO;
 
 	[TestFixture]
 	public sealed class OTBMWorldLoaderTests {
+		public const int OTBMIdentifierLength = 4;
+		public enum Worlds { Empty }
 
 		[Test]
 		public void LoadEmptyWorld() {
-			throw new NotImplementedException();
+			var rawData = GetSerializedWorldData(Worlds.Empty);
+			var relevantData = rawData.Slice(OTBMIdentifierLength);
+			var parsingTree = OTBMWorldLoader.LoadWorld(relevantData);
+		}
+
+		public static Memory<byte> GetSerializedWorldData(Worlds world) {
+			var testWorldsDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory,
+			"..", "..", "..", "..", "OTBMTestWorlds");
+
+			switch (world) {
+				case Worlds.Empty:
+				return File.ReadAllBytes(Path.Combine(testWorldsDirectory, "Empty.otbm"));
+
+				default:
+				throw new InvalidOperationException();
+			}
 		}
 	}
 }
