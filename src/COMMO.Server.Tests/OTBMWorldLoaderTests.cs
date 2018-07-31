07eac8d488946ea3944b44f1f3a4fb7147192667
MirandaTZ <>
2018-07-31 15:45:28 -0300

    Created classes with tests and data for OTBMWorldLoader.

---
+++ src/COMMO.Server.Tests/OTBMWorldLoaderTests.cs
@@ -0,0 +1,14 @@
+namespace COMMO.Server.Tests {
+	using NUnit.Framework;
+	using System;
+
+	[TestFixture]
+	public sealed class OTBMWorldLoaderTests {
+
+		[Test]
+		public void LoadEmptyWorld() {
+			throw new NotImplementedException();
+		}
+	}
+}
+
